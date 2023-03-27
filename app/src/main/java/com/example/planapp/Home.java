01f0911e6d6b9144a25598a63e7226d5ee16c844
jonat <jonathanu064@yahoo.com>
2023-03-27 18:55:59 -0500

    Completed login and registration functions along with logout button

---
+++ app/src/main/java/com/example/planapp/Home.java
@@ -0,0 +1,57 @@
+package com.example.planapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.planapp.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Home extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    FirebaseUser user;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        user = auth.getCurrentUser();
+        textView = findViewById(R.id.displayUser);
+        if (user == null)
+        {
+            Intent intent = new Intent(getApplicationContext(),Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
