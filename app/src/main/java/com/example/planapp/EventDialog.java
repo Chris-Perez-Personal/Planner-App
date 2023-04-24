b0c61dbf202adfbe4548019701549df9e82dea26
Chris Perez <94135693+Chris-Perez-Personal@users.noreply.github.com>
2023-04-24 03:16:40 -0500

    Edited methods to handle time and date data and validate them.
    Signed-off-by: Chris Perez <anc332@txstate.edu>

--- app/src/main/java/com/example/planapp/EventDialog.java
+++ app/src/main/java/com/example/planapp/EventDialog.java
@@ -1,16 +1,22 @@
 package com.example.planapp;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -36,20 +42,25 @@ public class EventDialog extends DialogFragment {
 
         EditText nameEditText = view.findViewById(R.id.nameEditText);
         EditText descriptionEditText = view.findViewById(R.id.descriptionEditText);
-        EditText startTimeEditText = view.findViewById(R.id.startTimeEditText);
-        EditText endTimeEditText = view.findViewById(R.id.endTimeEditText);
+        TimePicker startTimePicker = view.findViewById(R.id.startTimePicker);
+        TimePicker endTimePicker = view.findViewById(R.id.endTimePicker);
         Button saveButton = view.findViewById(R.id.saveButton);
 
         saveButton.setOnClickListener(v -> {
             String name = nameEditText.getText().toString().trim();
             String description = descriptionEditText.getText().toString().trim();
-            String startTime = startTimeEditText.getText().toString().trim();
-            String endTime = endTimeEditText.getText().toString().trim();
 
-            if (name.isEmpty() || description.isEmpty() || startTime.isEmpty() || endTime.isEmpty()) {
+            // Get the selected start and end times from the time picker
+            String startTime = String.format("%02d:%02d", startTimePicker.getHour(), startTimePicker.getMinute());
+            String endTime = String.format("%02d:%02d", endTimePicker.getHour(), endTimePicker.getMinute());
+
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(description)) {
                 Toast.makeText(getActivity(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            } else if (isValidTime(startTime) || isValidTime(endTime)) {
+                Toast.makeText(getActivity(), "Invalid time format", Toast.LENGTH_SHORT).show();
             } else {
                 Event event = new Event(name, description, startTime, endTime);
+                assert getArguments() != null;
                 databaseReference.child(getArguments().getString("dateSelected")).push().setValue(event);
                 dismiss();
             }
@@ -64,5 +75,10 @@ public class EventDialog extends DialogFragment {
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.setTitle("New Event");
         return dialog;
+    }
+
+    // Helper method to check if a time string is valid
+    private boolean isValidTime(String time) {
+        return !time.matches("^([01][0-9]|2[0-3]):[0-5][0-9]$");
     }
 }
