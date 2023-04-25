ede47a38ad19efe6cb91747f0736a3d418c60190
Chris Perez <anc332@txstate.edu>
2023-04-25 03:13:20 -0500

    Init commit
    Signed-off-by: Chris Perez <anc332@txstate.edu>

---
+++ app/src/main/java/com/example/planapp/EventAdapter.java
@@ -0,0 +1,55 @@
+package com.example.planapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private List<Event> eventList;
+
+    public EventAdapter(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.event_item, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.eventNameTextView.setText(event.getName());
+        holder.eventDescriptionTextView.setText(event.getDescription());
+        holder.eventTimeTextView.setText(event.getStartTime() + " - " + event.getEndTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        public TextView eventNameTextView;
+        public TextView eventDescriptionTextView;
+        public TextView eventTimeTextView;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            eventNameTextView = itemView.findViewById(R.id.eventNameTextView);
+            eventDescriptionTextView = itemView.findViewById(R.id.eventDescriptionTextView);
+            eventTimeTextView = itemView.findViewById(R.id.eventTimeTextView);
+        }
+    }
+}
+
